7271c2a70fe94d87b279ac10a6882549d89d71b7
Diego Mierez <dmierez@gmail.com>
2020-05-11 22:33:44 -0300

    Autenticacion de usuarios y registro desde Google

---
+++ public/js/auth/autenticacion.js
@@ -0,0 +1,75 @@
+class Autenticacion {
+  autEmailPass (email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).then(result => {
+      if (result.user.emailVerified) {
+        $('#avatar').attr('src', 'imagenes/usuario_auth.png')
+        Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
+      } else {
+        firebase.auth().signOut()
+        Materialize.toast(
+          `Por favor realiza la verificación de la cuenta`,
+          5000
+        )
+      }
+    })
+
+    $('.modal').modal('close')
+  }
+
+  crearCuentaEmailPass (email, password, nombres) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        result.user.updateProfile({
+          displayName: nombres
+        })
+
+        const configuracion = {
+          url: 'http://localhost:5500/public'
+        }
+
+        result.user.sendEmailVerification(configuracion).catch(error => {
+          console.error(error)
+          Materialize.toast(error.message, 4000)
+        })
+
+        firebase.auth().signOut()
+
+        Materialize.toast(
+          `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
+          4000
+        )
+
+        $('.modal').modal('close')
+      })
+      .catch(error => {
+        console.error(error)
+        Materialize.toast(error.message, 4000)
+      })
+  }
+
+  authCuentaGoogle () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider)
+    .then (result => {
+      $('#avatar').attr('src', result.user.photoURL)
+      $('.modal').modal('close')
+      Materialize.toast(`Hola ${result.user.displayName} bienvenido a escueLean`, 4000)
+    })
+    .catch (error => {
+      console.error(error)
+      Materialize.toast(`Error al autenticarse con Google: ${error}`, 40000)
+    })
+  }
+
+  authCuentaFacebook () {
+    // $('#avatar').attr('src', result.user.photoURL)
+    // $('.modal').modal('close')
+    // Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authTwitter () {
+    // TODO: Crear auth con twitter
+  }
+}
